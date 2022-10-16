67cd8b716cc03b3aad913173247a4ad1aec34ec5
CarlosRoberto <crnl@aluno.ifnmg.edu.br>
2022-10-16 15:39:37 -0300

    Esqueleto do projeto

---
+++ src/application/Program.java
@@ -0,0 +1,11 @@
+package application;
+import views.Home;
+
+public class Program {
+    public static void main(String[] args) {
+        System.out.println("Olá");
+        Home tela = new Home();
+        tela.setVisible(true);
+    }
+    
+}
